9ecd7cf26c056e32f476aa09ce02b9a153f95495
Gelson Matavela <gelson.matavela@uzambeze.ac.mz>
2025-11-16 22:32:32 +0200

    chore(byte-stream) - concluse byte code to stream data from source txt file to dest txt file

--- src/ByteStreamExample.java
+++ src/ByteStreamExample.java
@@ -5,8 +5,26 @@ import java.io.IOException;
 class ByteStreamExample {
     public static void main(String[] ar) throws IOException {
         FileInputStream inStream = null;
-        FileOutputStream outputStream = null;
+        FileOutputStream outStream = null;
 
-        
+        try {
+            inStream = new FileInputStream("src/source.txt");
+            outStream = new FileOutputStream("src/dest.txt");
+            
+            // faz leitura de byte a cada tempo, se atingiu o final do ficheiro retorna -1
+            int content;
+
+            while ((content = inStream.read()) != -1){
+                outStream.write((byte) content);
+            }
+
+        } finally {
+            if(inStream != null){
+                inStream.close();
+            }
+            if(outStream != null){
+                outStream.close();
+            }
+        }
     }
 }
