e10aef69b402ba160436dc7ed3857057a55d7450
Gelson Matavela <gelson.matavela@uzambeze.ac.mz>
2025-11-16 22:09:04 +0200

    chore(byte-stream) - add real I/O stream code example

---
+++ src/ByteStreamExample.java
@@ -0,0 +1,12 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+class ByteStreamExample {
+    public static void main(String[] ar) throws IOException {
+        FileInputStream inStream = null;
+        FileOutputStream outputStream = null;
+
+        
+    }
+}
---
+++ src/CharaterStreamDemo.java
@@ -0,0 +1,9 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+public class CharaterStreamDemo {
+    public static void main(String[] args) throws IOException {
+      FileReader readerStream = null;
+      FileWriter writerStream = null;
+    }
+}
