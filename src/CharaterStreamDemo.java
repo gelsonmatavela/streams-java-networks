41a3fe2a75e6e32aad3c24ea19fc76e70fbe9b95
Gelson Matavela <gelson.matavela@uzambeze.ac.mz>
2025-11-16 22:46:28 +0200

    chore(character-stream) - create function to send in character datas from source txt file to dest txt file

--- src/CharaterStreamDemo.java
+++ src/CharaterStreamDemo.java
@@ -3,7 +3,26 @@ import java.io.FileWriter;
 import java.io.IOException;
 public class CharaterStreamDemo {
     public static void main(String[] args) throws IOException {
-      FileReader readerStream = null;
-      FileWriter writerStream = null;
+      FileReader characterReaderStream = null;
+      FileWriter characterWriterStream = null;
+
+      try{
+         characterReaderStream = new FileReader("src/characterSource.txt");
+         characterWriterStream = new FileWriter("src/characterDest.txt");
+
+        // Reading source file and wrinting content to target file character by character
+        
+        int content;
+        while((content = characterReaderStream.read()) != -1){
+            characterWriterStream.append((char) content);
+        }
+      }finally{
+        if (characterReaderStream != null){
+            characterReaderStream.close();
+        }
+        if(characterWriterStream != null){
+            characterWriterStream.close();
+        }
+      }
     }
 }
